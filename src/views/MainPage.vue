66bf346d6a14a8ada9baf9f6d73c830ba0f7e3a1
Lucas Gabriel <lucas1020111940@hotmail.com>
2024-01-09 19:37:46 -0300

    correction

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -65,7 +65,7 @@
     </div>
     <div class="w-1/2 dim:w-full max-h-full">
       <div class="flex items-center justify-center h-full bg-light-khaki dim:bg-mossgreen p-2">
-        <div class="rounded-md w-full h-full bg-mossgreen dim:bg-none">
+        <div class="rounded-md w-3/4 h-3/4 dim:w-full dim:h-full bg-mossgreen dim:bg-none">
          <div class="flex items-center justify-center w-full h-full">
             <iframe  class="w-3/4 h-3/4 aspect-video dim:rounded-md" src="https://www.youtube.com/embed/q1pBwQl6zZ0?si=3Q5_KldJpdQTfGFZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
