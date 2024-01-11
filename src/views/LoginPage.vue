b8fb54ef46fbe1abc96d89d4840ac11b4b7685a2
Lucas Gabriel <lucas1020111940@hotmail.com>
2024-01-11 03:03:27 -0300

    att login and register

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,30 +1,30 @@
 <template>
     <MainBar class="w-full top-0 z-50" />
     <div class="min-h-screen flex items-center justify-center">
-        <img :src="bglogin" alt="" class="object-cover w-full h-full"/>
+        <img :src="bglogin" alt="" class="object-cover w-full h-full" />
         <!-- <video autoplay loop muted playsinline class="absolute object-cover w-full h-full">
             <source :src="river" type="video/mp4" />
             Seu navegador não suporta a tag de vídeo.
         </video> -->
 
         <!-- <div class="absolute inset-0 bg-black opacity-50"></div> -->
-        <div class="flex w-full min-h-full justify-center absolute">
+        <div
+            class="flex w-1/2 min-h-full justify-center absolute bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 shadow-lg">
             <div class="flex justify-center">
                 <div class="flex absolute min-h-full flex-1 flex-col justify-center lg:px-8">
-                    <div class="w-auto h-auto bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100
-                    px-16 py-12 shadow-lg">
+                    <div class="flex flex-col w-auto h-auto">
                         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                            <img class="mx-auto h-32 w-auto size-20"  alt="" :src="acampicon"/>
+                            <img class="mx-auto h-32 w-auto size-20" alt="" :src="acampicon" />
                             <h2 class="text-center text-2xl font-bold leading-9 tracking-tight text-[#fff]">
                                 Entrar
                             </h2>
                         </div>
 
-                        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                            <form class="space-y-6" @submit.prevent="getHome">
+                        <div class="flex flex-col mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                            <form class="space-y-6 w-auto h-auto" @submit.prevent="getHome">
                                 <div class="relative z-0">
                                     <input type="text" id="user"
-                                        class="block py-2.5 px-0 w-full text-lg text-white bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-black dark:focus:border-orange-700 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+                                        class="block py-2.5 px-6 w-full text-lg text-white bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-black dark:focus:border-orange-700 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
                                         placeholder=" " />
                                     <label for="user"
                                         class="absolute text-lg text-black dark:text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Usuário</label>
@@ -38,7 +38,7 @@
                                 </div>
                                 <div>
                                     <button type="submit"
-                                        class="flex w-full justify-center rounded-md bg-olive-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                        class="flex w-full justify-center rounded-md bg-olive-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-mossgreen focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-mossgreen">
                                         Entrar
                                     </button>
                                 </div>
@@ -56,18 +56,17 @@
             </div>
         </div>
     </div>
-    <Footer/>
+    <Footer />
 </template>
 
 <script setup>
-import acampicon from './../assets/image/acamp-icon.png';
-import MainBar from './../components/MainBar.vue';
-import Footer from './../components/Footer.vue';
-import router from '../router';
-import bglogin from './../assets/image/bglogin.jpg'
-
+import acampicon from "./../assets/image/acamp-icon.png";
+import MainBar from "./../components/MainBar.vue";
+import Footer from "./../components/Footer.vue";
+import router from "../router";
+import bglogin from "./../assets/image/bglogin.jpg";
 
 function getHome() {
-    router.push({ path: '/' });
+    router.push({ path: "/" });
 }
 </script>
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,14 +1,154 @@
 <template>
-  <MainBar/>
-  <div>a</div>
-  <!-- <Footer/> -->
+  <MainBar />
+  <div>
+    <div class="min-h-screen flex items-center justify-center">
+      <img :src="bglogin" alt="" class="object-cover w-full h-full" />
+
+      <div
+        class="flex w-1/2 min-h-full absolute justify-center items-center bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 shadow-lg"
+      >
+        <div class="flex h-auto w-auto flex-col justify-center lg:px-8">
+          <div class="flex flex-col justify-center items-center w-auto h-auto sm:mx-auto sm:max-w-smsm:mx-auto sm:w-full sm:max-w-sm">
+            <img class="mx-auto h-32 w-auto size-20" alt="" :src="acampicon" />
+            <h1 class="text-olive-black font-bold text-3xl font-pathway leading-tight">Crie sua Conta</h1>
+            <h5 class="text-olive-black font-semibold text-center font-pathway hyphens-auto leading-tight text-lg">
+              Seja bem-vindo! Complete o formulário abaixo para começar.
+            </h5>
+          </div>
+          <div class="w-auto h-auto mt-2">
+            <form class="max-w-md mx-auto">
+              
+              <div class="relative z-0 w-full mb-5 group">
+                <input
+                  type="text"
+                  name="user"
+                  id="user"
+                  class="block py-2.5 px-6 w-full text-lg text-white bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-black dark:focus:border-orange-700 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="user"
+                  class="absolute text-lg text-black dark:text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                  >Usuário</label
+                >
+              </div>
+              <div class="relative z-0 w-full mb-5 group">
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  class="block py-2.5 px-6 w-full text-lg text-white bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-black dark:focus:border-orange-700 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="password"
+                  class="absolute text-lg text-black dark:text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                  >Senha</label
+                >
+              </div>
+              <div class="relative z-0 w-full mb-5 group">
+                <input
+                  type="password"
+                  name="repeat_password"
+                  id="repeat_password"
+                  class="block py-2.5 px-6 w-full text-lg text-white bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-black dark:focus:border-orange-700 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="repeat_password"
+                  class="absolute text-lg text-black dark:text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                  >Confirmar senha</label
+                >
+              </div>
+              <div class="relative z-0 w-full h-auto mb-5 group">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  class="block py-2.5 px-6 w-full text-lg text-white bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-black dark:focus:border-orange-700 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="email"
+                  class="absolute text-lg text-black dark:text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                  >Email
+                </label>
+              </div>
+              <div class="grid md:grid-cols-2 md:gap-6">
+                <div class="relative z-0 w-full mb-5 group">
+                  <input
+                    type="text"
+                    name="first_name"
+                    id="first_name"
+                    class="block py-2.5 px-6 w-full text-lg text-white bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-black dark:focus:border-orange-700 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    for="first_name"
+                    class="absolute text-lg text-black dark:text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                    >Nome</label
+                  >
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                  <input
+                    type="text"
+                    name="last_name"
+                    id="last_name"
+                    class="block py-2.5 px-6 w-full text-lg text-white bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-black dark:focus:border-orange-700 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    for="last_name"
+                    class="absolute text-lg text-black dark:text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                    >Sobrenome</label
+                  >
+                </div>
+              </div>
+              <div class="grid md:grid-cols-2 md:gap-6">
+                <div class="relative z-0 w-full mb-5 group">
+                  <input
+                    type="tel"
+                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    name="phone"
+                    id="phone"
+                    class="block py-2.5 px-6 w-full text-lg text-white bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-black dark:focus:border-orange-700 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    for="phone"
+                    class="absolute text-lg text-black dark:text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                    >Celular</label
+                  >
+                </div>
+                
+              </div>
+              <button
+                type="submit"
+                class="flex w-full justify-center rounded-md bg-olive-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-500"
+              >
+                Criar Conta
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <Footer />
 </template>
 
 <script setup>
-import MainBar from '../components/MainBar.vue';
-import Footer from '../components/Footer.vue';
+import MainBar from "../components/MainBar.vue";
+import Footer from "../components/Footer.vue";
+import bglogin from "./../assets/image/bglogin.jpg";
+import acampicon from "./../assets/image/acamp-icon.png";
 </script>
 
-<style>
-
-</style>
+<style></style>
